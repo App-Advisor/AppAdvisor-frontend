e2bdb252ef90db16cc012f877d64d696d824ab33
raparisonbryan <raparisonbryan@yahoo.fr>
2024-03-04 10:11:53 +0100

    fix header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,27 @@
 import styles from "./page.module.scss";
+import zigzag from "@/assets/zigzag.svg";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>coucou</h1>
-      <h2>coucou</h2>
-      <p>coucou</p>
-    </main>
+  <main className={styles.main}>
+    <div className={styles.hero}>
+      <div className={styles.hero_mask}/>
+      <div className={styles.hero_container}>
+        <div className={styles.zigzag}>
+          <Image fill src={zigzag} style={{objectFit:"cover"}}/>
+        </div>
+        <h1>Découvrez les meilleures solutions pour votre développement web</h1>
+        <form className={styles.form}>
+          <input type="text" placeholder="Recherchez votre framework, outil, ..."/>
+          <input type="button" className={styles.button} value="Rechercher" />
+        </form>
+      </div>
+    </div>
+
+    <div className={styles.tool_list}>
+      <h2>Selection d'avis sur les outils</h2>
+    </div>
+  </main>
   );
 }
---
+++ src/components/OutilCard/OutilCard.jsx
@@ -0,0 +1,7 @@
+export default function OutilCard() {
+    return (
+        <div className={style.card}>
+
+        </div>
+    )
+}
